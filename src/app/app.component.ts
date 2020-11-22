849eb5676954152fd3a610b7ec10ba27206ef9ad
Lasanija <yuliahladunyk@gmail.com>
2020-11-22 19:01:29 +0200

    task25

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Task } from './task.model';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,33 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
+  homework: Task[]=[{
+    id: 1,
+    name: 'task1',
+    status: false
+  }, {
+    id: 2,
+    name: 'task2',
+    status: true
+  }, {
+    id: 3,
+    name: 'task3',
+    status: false
+  }, {
+    id: 4,
+    name: 'task4',
+    status: true
+  }, {
+    id: 5,
+    name: 'task5',
+    status: true
+  }];
+  ngOnInit(){
+    console.log(this.calcPrice(1000,30))
+  }
+  calcPrice(price:number, sale:number):number{
+    price-=price*sale/100;
+    return price;
+  }
 
 }
